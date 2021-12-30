a2ca86511ca43900bcbfd78a2b5393ddf5e4167e
Vladimir Kompaniec <vkompaniec@gmail.com>
2021-12-30 21:57:37 +0300

    Добавил модальные компоненты

---
+++ src/components/TemperatureModal.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {View, ViewStyle, StyleSheet, Text} from 'react-native';
+import {convertHeight, convertWidth} from '../utils/dimensions';
+import Modal from 'react-native-modal';
+
+interface TemperatureModalProps {
+    style?: ViewStyle;
+    isVisible: boolean;
+    onClose: () => void;
+}
+
+const TemperatureModal: React.FC<TemperatureModalProps> = ({
+                                                               style = {},
+                                                               isVisible,
+                                                               onClose,
+                                                           }) => {
+
+    const [temperature, setTemperature] = useState(0);
+
+    const wheelPickerData = ['0', '1', '2'];
+
+    return (
+        <Modal isVisible={isVisible} style={styles.container} onBackButtonPress={onClose} onBackdropPress={onClose}>
+            <View style={StyleSheet.flatten([styles.modal, style])}>
+                <Text style={styles.title}>Задайте температуру</Text>
+
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modal: {
+        width: '90%',
+        paddingTop: convertHeight(16),
+        paddingBottom: convertHeight(32),
+        backgroundColor: 'white',
+        alignItems: 'center',
+        borderRadius: convertWidth(8),
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black',
+        marginBottom: convertHeight(32),
+    },
+    wheelPicker: {}
+});
+
+export default TemperatureModal;
